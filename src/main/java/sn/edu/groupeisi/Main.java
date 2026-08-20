c162fcecedb289a6d577f85ba192ceec4132bdb7
Samba SIDIBE <sambasidibe@gmail.com>
2026-08-20 20:34:20 +0000

    initialisation projet et configuration BD

---
+++ src/main/java/sn/edu/groupeisi/Main.java
@@ -0,0 +1,18 @@
+package sn.edu.groupeisi;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        EntityManagerFactory emf = Persistence
+                .createEntityManagerFactory("groupeisi");
+        EntityManager em = emf.createEntityManager();
+
+        em.close();
+        emf.close();
+    }
+}
